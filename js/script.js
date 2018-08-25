ca766e08a97d938c3d84c291e7d370803b001783
Victor Chan <vchan0717@gmail.com>
2018-08-25 01:23:30 -0400

    Some changes to JS

--- js/script.js
+++ js/script.js
@@ -6,7 +6,12 @@ FSJS project 2 - List Filter and Pagination
 // Add variables that store DOM elements you will need to reference and/or manipulate
 const studentList = document.querySelectorAll(".student-item"); // Holds all tags with the class 'student-item'
 let totalPages = Math.ceil(studentList.length / 10);
-let div = document.createElement("div");
+const div = document.createElement("div");
+let studentArray = []; // Holds list of students.
+const searchButton = document.querySelector("button"); // Selects the search button
+const searchInput = document.querySelector("input"); // Selects input field
+const studentEmail = document.querySelectorAll(".email"); // Selects all tags with 'email' class
+const studentName = document.querySelectorAll("h3"); // Selects all h3 tags
 
 /*
 Function to show student list depending on page.
@@ -14,18 +19,19 @@ Checks to see if the student list element exceeds 10.
 */
 const showPage = (page, list) => {
   for (let i = 0; i < list.length; i++) {
+    list[i].style.display = "none";
     if (i >= page * 10 - 10 && i < page * 10) {
       list[i].style.display = "block";
       studentArray.pop(list[i]); // Removes previous students for search function
-    } else list[i].style.display = "none";
+    }
   }
 };
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
 const appendPageLinks = list => {
+  div.innerHTML = "";
   totalPages = Math.ceil(list.length / 10);
   // Create a div, give it the 'pagination' class and append to .page div.
-  div = document.createElement("div");
   div.className = "pagination";
   const pageDiv = document.querySelector(".page");
   pageDiv.appendChild(div);
@@ -49,7 +55,6 @@ const appendPageLinks = list => {
       const aList = document.getElementsByTagName("a"); // Holds list of a tags
       // for loop to loop through a tags and remove the 'active' class.
       showPage(page, list);
-      div.innerHTML = "";
       for (let i = 0; i < aList.length; i++) {
         aList[i].classList.remove("active");
         event.target.classList.add("active"); // adds class 'active' to event.target
@@ -58,25 +63,25 @@ const appendPageLinks = list => {
   }
 };
 
-// Search Function
-const searchButton = document.querySelector("button"); // Selects the search button
-const searchInput = document.querySelector("input"); // Selects input field
-const studentEmail = document.querySelectorAll(".email"); // Selects all tags with 'email' class
-const studentName = document.querySelectorAll("h3"); // Selects all h3 tags
-let studentArray = []; // Holds list of students.
+showPage(1, studentList);
+appendPageLinks(studentList);
 
-searchButton.addEventListener("click", () => {
+// Search Function to find if names match input name search
+const searchList = () => {
   for (let i = 0; i < studentList.length; i++) {
     if (
-      studentEmail[i].textContent.search(searchInput.value) > -1 ||
-      studentName[i].textContent.indexOf(searchInput.value) > -1
+      studentEmail[i].textContent.toLowerCase().indexOf(searchInput.value) >
+        -1 ||
+      studentName[i].textContent.toLowerCase().indexOf(searchInput.value) > -1
     ) {
       studentArray.push(studentList[i]);
     } else studentList[i].style.display = "none";
   }
-  div.innerHTML = ""; // Removes content inside div so it does not create extra pagination divs on click.
-  showPage(1, studentArray);
-});
+};
 
-showPage(1, studentList);
-appendPageLinks(studentList);
+// Search Button functionality.
+searchButton.addEventListener("click", () => {
+  searchList();
+  showPage(1, studentArray);
+  appendPageLinks(studentArray);
+});
